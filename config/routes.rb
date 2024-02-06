270e7cc447ac92bb984a685a89e7005d01b1d3f0
Daniel Mauro <daniel.mauro@asurion.com>
2024-02-06 14:55:23 -0600

    database

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails
+  .application
+  .routes
+  .draw do
+    namespace :api do
+      namespace :v1 do
+        resources :products, only: %i[index show create]
+      end
+    end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-end
+    # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+    # Can be used by load balancers and uptime monitors to verify that the app is live.
+    get 'up' => 'rails/health#show', :as => :rails_health_check
+
+    # Defines the root path route ("/")
+    # root "posts#index"
+  end
